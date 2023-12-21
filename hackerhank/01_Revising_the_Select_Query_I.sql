4b411c2f2eed3fd557d33da6c0b53509594c6442
Sofia Popsin <sofia.popsin99@gmail.com>
2023-12-20 23:06:40 -0300

    add challenge specs

--- hackerhank/01_Revising_the_Select_Query_I.sql
+++ hackerhank/01_Revising_the_Select_Query_I.sql
@@ -1,3 +1,6 @@
+/*
+Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+*/ 
  CREATE TABLE CITY{
     ID NUMBER,
     NAME VARCHAR2 (17),
